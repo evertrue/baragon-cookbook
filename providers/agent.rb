28fa1663b34e1554a913d708f6762280eb833d2c
Jeff Byrnes <jeff@evertrue.com>
2015-09-26 10:01:25 -0400

    Add header to recipes, resources, providers
    Nice to have

--- providers/agent.rb
+++ providers/agent.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: baragon
+# Provider:: agent
+#
+# Copyright 2014, EverTrue, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 action :create do
   # Take the agent_yaml from node the attributes as a template and customize it
   agent_yaml = JSON.parse(node[:baragon][:agent_yaml].to_json)
