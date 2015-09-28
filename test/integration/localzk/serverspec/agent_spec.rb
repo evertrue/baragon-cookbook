8a56b38adc22c5ffe6cf77f9d973c8d8dec294f8
Jeff Byrnes <jeff@evertrue.com>
2015-09-28 16:57:25 -0400

    Rewrite Serverspec assertions to be more readable
    English is nice, sometimes.

--- test/integration/localzk/serverspec/agent_spec.rb
+++ test/integration/localzk/serverspec/agent_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe 'Baragon agent' do
-  describe port 8882 do
-    it { is_expected.to be_listening.with('tcp') }
+  it 'is listening on the specified port' do
+    expect(port 8882).to be_listening.with('tcp')
   end
 
-  it 'has a running and enabled baragon-agent service' do
+  it 'is running and enabled' do
     expect(service('baragon-agent-default')).to be_enabled
     expect(service('baragon-agent-default')).to be_running
   end
