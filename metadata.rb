8e03106e1255ed2bbb469bbc7490b05e43adca00
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-07-15 10:43:01 -0400

    Bump to v5.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures baragon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '4.0.0'
+version          '5.0.0'
 issues_url       'https://github.com/evertrue/baragon-cookbook/issues'
 source_url       'https://github.com/evertrue/baragon-cookbook'
 
