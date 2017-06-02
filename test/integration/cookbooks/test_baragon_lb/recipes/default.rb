52215b0c34dd8b85a46a03c736b9e63f17763206
Eric Herot <eric.github@herot.com>
2017-06-02 13:58:58 -0400

    Force nginx to restart before creating our test baragon request
    Needed because otherwise the PID file doesn't get properly written and Baragon can't find the nginx process

--- test/integration/cookbooks/test_baragon_lb/recipes/default.rb
+++ test/integration/cookbooks/test_baragon_lb/recipes/default.rb
@@ -3,5 +3,6 @@ Chef::Resource.send(:include, Baragon::Helpers)
 ruby_block 'create test zk request' do
   block { create_test_zk_request }
   action :nothing
+  notifies :restart, 'service[nginx]', :before
   subscribes :run, 'service[baragon-server]'
 end
