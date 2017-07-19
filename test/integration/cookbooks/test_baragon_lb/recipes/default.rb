053e854d8fd0d279bd1c4b7d71ad48e5dda2c605
Eric Herot <eric.github@herot.com>
2017-07-19 18:35:24 -0400

    Add a working upstream server to our proxy test rig

--- test/integration/cookbooks/test_baragon_lb/recipes/default.rb
+++ test/integration/cookbooks/test_baragon_lb/recipes/default.rb
@@ -1,8 +1,33 @@
 Chef::Resource.send(:include, Baragon::Helpers)
 
+cookbook_file '/etc/nginx/conf.d/baragon.conf' do
+  action :nothing
+  subscribes :create, 'ruby_block[create test zk request]'
+  notifies :restart, 'service[nginx]', :delayed
+end
+
 ruby_block 'create test zk request' do
   block { create_test_zk_request }
   action :nothing
   notifies :restart, 'service[nginx]', :before
   subscribes :run, 'service[baragon-server]'
+end
+
+package 'lighttpd'
+
+service 'lighttpd' do
+  supports restart: true
+  action :enable
+end
+
+cookbook_file '/etc/lighttpd/lighttpd.conf' do
+  notifies :restart, 'service[lighttpd]'
+end
+
+directory '/var/www/html/testbasepath1' do
+  recursive true
+end
+
+file '/var/www/html/testbasepath1/index.html' do
+  content "<h1>Hello World!</h1>\n"
 end
