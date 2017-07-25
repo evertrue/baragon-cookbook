8357f81d30b55fc465d08ba92ed37e4fece52b72
Eric Herot <eric.github@herot.com>
2017-07-25 19:14:51 -0400

    Test that custom headers are present in config AND response

--- test/integration/agent-service/serverspec/agent_spec.rb
+++ test/integration/agent-service/serverspec/agent_spec.rb
@@ -8,6 +8,13 @@ describe 'Baragon agent' do
   end
 
   describe 'dynamic proxies and upstreams created correctly' do
+    custom_response_headers = {
+      'Strict-Transport-Security' => 'max-age=31536000; includeSubDomains;',
+      'Cache-Control' => 'value should not be replaced',
+      'Pragma' => 'no-cache',
+      'X-Frame-Options' => 'DENY'
+    }
+
     describe file '/tmp/default/proxy/tk-test-service.conf' do
       it { is_expected.to be_file }
 
@@ -15,6 +22,22 @@ describe 'Baragon agent' do
         subject { super().content }
         it { is_expected.to match(%r(^location  /testbasepath1 {$)) }
         it { is_expected.to match(%r{^  proxy_pass http://baragon_default_tk-test-service;}) }
+
+        custom_response_headers.keys.each do |header|
+          it { is_expected.to match(%r{^  add_header #{header} \$#{header.downcase.gsub('-', '_')}_custom_header always;})}
+        end
+      end
+    end
+
+    describe 'HTTP response' do
+      custom_response_headers.each do |header, value|
+        it "return header #{header} with value \"#{value}\"" do
+          expect(
+            Net::HTTP.new('localhost', 8443).get(
+              '/testbasepath1/', 'Host' => 'test.local'
+            )[header.downcase]
+          ).to eq value
+        end
       end
     end
 
